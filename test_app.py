fe86cc383732c8a26eb4d0a58202ac3556a56f88
hero_kartiko <96468870+herkart1011@users.noreply.github.com>
2025-04-06 18:41:52 +0000

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -4,5 +4,5 @@ from app import hello
 class TestApp(unittest.TestCase):
     def test_hello(self):
         self.assertEqual(hello(),"Hello, World!")
-if __name__ = "__main__":
+if __name__ == "__main__":
     unittest.main()
